25a677f0ccdc336bafc77259aa46112594f0f3a4
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-11-09 15:28:19 +0530

    chap2

---
+++ Chap2/strings.rb
@@ -0,0 +1,13 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts '\n'
+
+puts 'I like' + 'apple pie.'
+
+puts 'blink ' * 4
+
+puts "\n"
+puts 'You\'re swell!'
+puts "here\'s a \\ backslash'"
+
